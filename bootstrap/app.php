9a51d73b2313a82cb5cc249611c9635094b19291
LamishaShajnin <lamishashajnin83@gmail.com>
2026-04-09 15:55:33 +0600

    middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\RedirectIfAuthenticated;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,9 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'auth' => Authenticate::class,
+            'guest' => RedirectIfAuthenticated::class,
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+    
--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,25 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'index'])->name('home');
-Route::get('/account/register',[AccountController::class,'registration'])->name('account.registration');
-Route::post('/account/process-register',[AccountController::class,'processRegistration'])->name('account.processRegistration');
-Route::get('/account/login',[AccountController::class,'login'])->name('account.login');
 
-Route::post('/account/authenticate',[AccountController::class,'authenticate'])->name('account.authenticate');
-Route::get('/account/profile',[AccountController::class,'profile'])->name('account.profile');
-
-Route::get('/account/logout',[AccountController::class,'logout'])->name('account.logout');
 
 
 Route::group(['account'], function(){
+    //Guest Routes
+    Route::group(['middleware' => 'guest'],function(){
+        Route::get('/account/register',[AccountController::class,'registration'])->name('account.registration');
+        Route::post('/account/process-register',[AccountController::class,'processRegistration'])->name('account.processRegistration');
+        Route::get('/account/login',[AccountController::class,'login'])->name('account.login');
+        Route::post('/account/authenticate',[AccountController::class,'authenticate'])->name('account.authenticate');
+
+    });
+
+    Route::group(['middleware' => 'Auth'],function(){
+        Route::get('/account/profile',[AccountController::class,'profile'])->name('account.profile');
+        Route::get('/account/logout',[AccountController::class,'logout'])->name('account.logout');
+
+
+    });
 
 
 });
