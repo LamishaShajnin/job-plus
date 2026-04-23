a3694ecb94cf3e1b64573cd1923b19ec0fd698c9
LamishaShajnin <lamishashajnin83@gmail.com>
2026-04-23 16:07:24 +0600

    changed db

--- database/migrations/2026_04_23_085000_create_job_table.php
+++ database/migrations/2026_04_23_085000_create_job_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->foreignId('job_type_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->integer('vacancy');
             $table->string('salary')->nullable();
             $table->string('location');
@@ -28,6 +29,8 @@ return new class extends Migration
             $table->string('company_name');
             $table->string('company_location')->nullable();
             $table->string('company_website')->nullable();
+            $table->string('status');
+            $table->string('isFeatured');
             $table->timestamps();
             
         });
